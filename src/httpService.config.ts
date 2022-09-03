c4a9c7b22592cea51891fd9cd594c26ac62b228e
Jose Hernandez <jhernandez@azurian.com>
2022-09-03 16:50:48 -0400

    hgf

--- src/httpService.config.ts
+++ src/httpService.config.ts
@@ -6,7 +6,7 @@ export class HttpConfigService implements HttpModuleOptionsFactory {
     createHttpOptions(): HttpModuleOptions {
         return {
             headers: {
-                'Authorization': 'Bearer EAAGvZCoWRtiUBAPa5QZCGgTIDjrggjzAN6BobdaLPwFXZCm2gh9Cj5L2eT4rvn7sHewEsphWArYIyWZALdgkgn0e7mdZAepEF0EhLepMGY5nscJAyt02C6vKON1wbrTtwrY9S243jmaP1ThIvD1BcZCfUiuqrCAZBVY2LUszimziXtKIGZASYrFzZCsTdtEK6kUrUDDetPEZAdeGRKbSyKcghZB',
+                'Authorization': 'Bearer EAAGvZCoWRtiUBALFiJO0IX0WF1bRtYdQDsZCPwOvWiggS5ysGwOmntwJnxMioDNn8TVMWtZA6aaTdkV0MLMiOV0lqXu1hmA4nfkZCZAXoo54l9xikcMIymPe0mDxZCmQZBcj7YF3iYNNyOxckaQwa1ANzo68hrRKubpk9AhQtGnssh6TL47oMY4Xa6weYSY96F6zDY6Rj9LbXNnwqQnZAHD8',
                 'Content-Type': 'application/json'
             }
         }
