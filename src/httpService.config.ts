364e7d0eb33763db3ee1f5f54174b1f77123f2f1
Jose Hernandez <jhernandez@azurian.com>
2022-09-03 16:38:54 -0400

    hgf

--- src/httpService.config.ts
+++ src/httpService.config.ts
@@ -6,7 +6,7 @@ export class HttpConfigService implements HttpModuleOptionsFactory {
     createHttpOptions(): HttpModuleOptions {
         return {
             headers: {
-                'Authorization': 'Bearer EAAGvZCoWRtiUBAI7UQcejfnr0qwfrowTziG7l21p0KXILpotbowNdIdSuwp1hZByvze3WOiZB2DdOWkbGBQMN07ZCyBT4M696NZBx8b7ookUT5SLtdbUVF2mK0Ar7iZAkPD5TDtwGTuCeylHmT8LSsjWPtf5iZCe9c4l2YcenCjZBYYbggPBx2K97J0LnpYmgY5v82AKokOAQ5tRzsoWoZAGa',
+                'Authorization': 'Bearer EAAGvZCoWRtiUBAPa5QZCGgTIDjrggjzAN6BobdaLPwFXZCm2gh9Cj5L2eT4rvn7sHewEsphWArYIyWZALdgkgn0e7mdZAepEF0EhLepMGY5nscJAyt02C6vKON1wbrTtwrY9S243jmaP1ThIvD1BcZCfUiuqrCAZBVY2LUszimziXtKIGZASYrFzZCsTdtEK6kUrUDDetPEZAdeGRKbSyKcghZB',
                 'Content-Type': 'application/json'
             }
         }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -31,7 +31,7 @@ export class ProductsService {
 
   async sendMessage(request: WhatsappCloudApiRequest): Promise<AxiosResponse<WhatsappCloudAPIResponse>> {
     let headers: {
-      'Authorization': 'Bearer EAAGvZCoWRtiUBAI7UQcejfnr0qwfrowTziG7l21p0KXILpotbowNdIdSuwp1hZByvze3WOiZB2DdOWkbGBQMN07ZCyBT4M696NZBx8b7ookUT5SLtdbUVF2mK0Ar7iZAkPD5TDtwGTuCeylHmT8LSsjWPtf5iZCe9c4l2YcenCjZBYYbggPBx2K97J0LnpYmgY5v82AKokOAQ5tRzsoWoZAGa',
+      'Authorization': 'Bearer EAAGvZCoWRtiUBAPa5QZCGgTIDjrggjzAN6BobdaLPwFXZCm2gh9Cj5L2eT4rvn7sHewEsphWArYIyWZALdgkgn0e7mdZAepEF0EhLepMGY5nscJAyt02C6vKON1wbrTtwrY9S243jmaP1ThIvD1BcZCfUiuqrCAZBVY2LUszimziXtKIGZASYrFzZCsTdtEK6kUrUDDetPEZAdeGRKbSyKcghZB',
       'Content-Type': 'application/json'
     }
     const { data } = await firstValueFrom(this.httpService.post(this.baseUrl, request, {headers}));
