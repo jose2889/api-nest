f08febf8d20c61e17d030dc21274e34bcdb1aff7
edgardoll4 <edgardoll4@gmail.com>
2022-09-24 11:25:44 -0400

    cambio de id number

--- src/common/api-resource.ts
+++ src/common/api-resource.ts
@@ -1,5 +1,5 @@
 export const BASEURL = {
-    baseUrlWhatsappCloudApi: 'https://graph.facebook.com/v13.0/111256291694306/messages',
-    baseUrlWhatsappCloudApiProd: 'https://graph.facebook.com/v13.0/109463641900448/messages'
+    baseUrlWhatsappCloudApi: 'https://graph.facebook.com/v14.0/107490002110448/messages',
+    baseUrlWhatsappCloudApiProd: 'https://graph.facebook.com/v14.0/107490002110448/messages'
 }
     
