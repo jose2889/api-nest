c87b7caeb7b516aa43679f6a90b6c68cc6ed6582
edgardoll4 <edgardoll4@gmail.com>
2022-10-21 23:38:13 -0400

    modification of template " reservation notification "

--- src/products/whatsapp.controller.ts
+++ src/products/whatsapp.controller.ts
@@ -32,7 +32,7 @@ export class WhatsappController {
     let templateWhatsappApiRequest:WhatsappCloudApiRequest;
         templateWhatsappApiRequest = dataNotificationApiRequest;
 
-        templateWhatsappApiRequest.template.name = "notificacion_reservacion_cliente"; 
+        templateWhatsappApiRequest.template.name = "notification_reservation_client";//"notificacion_reservacion_cliente"; 
         templateWhatsappApiRequest.to = phoneNumber;
         templateWhatsappApiRequest.template.components[0].parameters[0].text = date;
         templateWhatsappApiRequest.template.components[0].parameters[1].text = businessName;   
