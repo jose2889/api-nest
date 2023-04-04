6a61cadca21b87dafb2beeae04c00c274e39d236
edgardoll4 <edgardoll4@gmail.com>
2023-04-04 11:10:21 -0400

    moddd

--- src/products/whatsapp.controller.ts
+++ src/products/whatsapp.controller.ts
@@ -56,7 +56,7 @@ export class WhatsappController {
       // }).catch((err) => {
       //     response.status(HttpStatus.BAD_REQUEST).json(err);
       // })
-      response.status = 201;
+      response.status = response.status(HttpStatus.CREATED);
   }
 
   @ApiResponse({ status: 201, description: 'Creado con éxito.'  })
